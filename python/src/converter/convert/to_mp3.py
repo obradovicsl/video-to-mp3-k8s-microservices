2a748daec6c01daba3bad9bf286bb5b4fb6fed75
obradovicsl <slobodanobradovic3@gmail.com>
2025-06-24 12:55:57 +0200

    Create converter service

---
+++ python/src/converter/convert/to_mp3.py
@@ -0,0 +1,48 @@
+import pika, json, tempfile, os
+
+import pika.spec
+from bson.objectid import ObjectId
+from moviepy.video.io.VideoFileClip import VideoFileClip
+
+def start(message, fs_videos, fs_mp3s, channel):
+    message = json.loads(message)
+
+    #empty temp file
+    tf = tempfile.NamedTemporaryFile()
+
+    #video contents
+    out = fs_videos.get(ObjectId(message["video_fid"]))
+
+    #add video contents to empty file
+    tf.write(out.read)
+
+    # convert video file (temp file) to audio
+    audio = VideoFileClip(tf.name).audio
+    tf.close()
+
+    #write audio to the file
+    tf_path = tempfile.gettempdir() + f"/{message['video_fid']}.mp3"
+    audio.write_audiofile(tf_path)
+
+    #save to mongo
+    f = open(tf_path, "rb")
+    data = f.read()
+    fid = fs_mp3s.put(data)
+    f.close()
+    os.remove(tf_path)
+
+    message["mp3_fid"] = str(fid)
+
+    try:
+        channel.basic_publish(
+            exchande="",
+            routing_key = os.environ.get("MP3_QUEUE"),
+            body=json.dumps(message),
+            properties=pika.BasicProperties(
+                delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
+            ),
+        )
+
+    except Exception as err:
+        fs_mp3s.delete(fid)
+        return "failed to publish message"
